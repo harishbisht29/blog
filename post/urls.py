4ae759e59089a2d4bcd8a5b66c9705cf94f6fad1
Harish Singh <harishbisht29@gmail.com>
2020-06-09 13:44:05 +0530

    create new page link and confirm post delete popup

--- post/urls.py
+++ post/urls.py
@@ -12,9 +12,15 @@ from .views import (
 urlpatterns = [
     path('post_detail/<slug:slug>/',postDetailView,name='post_detail'),
     path('post_list/',postListView,name='post_list'),
+    # 3 create urls -
+    # create -  Takes to template picker
+    # create_new - Creates a new article without using any template
+    # create- creates a new article with the help of template
+    
     path('create/', postTemlatePickerView, name='post_template_picker'),
-    path('create1/', postCreateView, name='post_create'),
-    path('create1/<str:template_id>', postCreateView, name='post_create'),
+    path('create_new/', postCreateView, name='post_create'),
+    path('create_new/<str:template_id>', postCreateView, name='post_create'),
+    
     path('update/<slug:slug>/',postUpdateView,name='post_update'),
     path('delete/<slug:slug>/',postDeleteView,name='post_delete'),
     path('suggest/',postSuggestView,name='post_suggest'),
